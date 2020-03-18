41fb736122a83779152e9152449d31d69bc65770
金光 彭 <413082318@qq.com>
2020-03-18 16:00:09 +0800

    test github

---
+++ Example/Pods/Target Support Files/JGPhoto/JGPhoto-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JGPhoto : NSObject
+@end
+@implementation PodsDummy_JGPhoto
+@end
---
+++ Example/Pods/Target Support Files/Pods-JGPhoto_Tests/Pods-JGPhoto_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JGPhoto_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JGPhoto_Tests
+@end
